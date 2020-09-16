a49911b75df49619d023d68831c49b2cdb66eed2
Michael Callahan <CallahanMichaelE@gmail.com>
2020-09-16 07:58:29 -0600

    updated server to now respond based on the text sent to it by adding if/else if statements for the body along with a body parser

--- server.js
+++ server.js
@@ -3,13 +3,26 @@ require('dotenv').config()
 const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.post('/sms', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.post('/', (req, res) => {
     const twiml = new MessagingResponse();
 
-    twiml.message('Test #2 - Oh, no! Look out behind you!')
+    let txtMessage = req.body.Body
+    let lowerCaseTxt = txtMessage.toLowerCase()
+
+    if (lowerCaseTxt == "code"){
+        twiml.message('Thank you so much! Please see my intro: https://www.youtube.com/watch?v=lRphR1oIVmA Type 1 for a list of the menu')
+    }else if (lowerCaseTxt == '1'){
+        twiml.message("Menu: 2 - Backstory 3 - Other stuff 4- Other Other stuff")
+    }else {
+        twiml.message("That is not the magic word")
+    }
+    
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
