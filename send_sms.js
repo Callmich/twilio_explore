85818efc61dbe142e9342ef8000946428aa9ed98
Michael Callahan <CallahanMichaelE@gmail.com>
2020-09-15 18:24:54 -0600

    It works! This is really freaking awesome

--- send_sms.js
+++ send_sms.js
@@ -1,1 +1,12 @@
 require('dotenv').config()
+
+const accountSid = process.env.ACCOUNT
+const authToken = process.env.AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken)
+
+client.messages
+    .create({
+        body: 'I am very excitied to see if this works!',
+        from: process.env.SEND_NUM,
+        to: process.env.REC_NUM
+    })
