276b8a18792a54956dbe7c15c174b66ed6935c69
Michael Callahan <CallahanMichaelE@gmail.com>
2020-09-23 11:31:01 -0600

    updated twilio responses in server

--- server.js
+++ server.js
@@ -19,32 +19,32 @@ app.post("/sms", (req, res) => {
   // A long set of if/else if statements to make sure our bases are covered on any potential incoming text. They also have links to the needed videos
   if (lowerCaseTxt == "code") {
     twiml.message(
-      "Thank you so much for joining me on this Choose Your Own Code Review!\nPlease check out my intro: https://youtu.be/5Luw_VfW25E \nText 1 for the menu... or try to make a guess to find the secret menu"
+      "Thank you so much for joining me on this Choose Your Own Code Review!\nPlease check out my intro: https://youtu.be/5Luw_VfW25E \nText 1 for the menu... or try to make a guess to find the secret menu."
     );
   } else if (lowerCaseTxt == "1") {
     twiml.message(
-      "Menu: \n1 - Menu (seems a bit recursive to choose this) \n2 - What is StoryBoard? \n3 - Design and Planning \n4- Migrations, Seeding, and Server \n5 - Routers \n6 - Middleware \n7 - Shared Functions \n8 - Documentation \n9 - Testing \n10 - The Next Steps \n11 - About Me"
+      "Menu: \n1 - Menu (seems a bit recursive) \n2 - What is StoryBoard? \n3 - Design and Planning \n4- Migrations, Seeding, and Server \n5 - Routers \n6 - Middleware \n7 - Shared Functions \n8 - Documentation \n9 - Testing \n10 - The Next Steps \n11 - About Me"
     );
   } else if (lowerCaseTxt == "2") {
-    twiml.message("What is StoryBoard? \nhttps://youtu.be/CwgIIxQRxMo");
+    twiml.message("What is StoryBoard? \nhttps://youtu.be/CwgIIxQRxMo \nHear a little bit of how this project started.");
   } else if (lowerCaseTxt == "3") {
-    twiml.message("Design and Planning: \nhttps://youtu.be/6_CTf5ZSccw");
+    twiml.message("Design and Planning: \nhttps://youtu.be/6_CTf5ZSccw \nDo you like database design? I do!");
   } else if (lowerCaseTxt == "4") {
-    twiml.message("Migrations, Seeding, and Server: \nhttps://youtu.be/38wDX84ZWVo");
+    twiml.message("Migrations, Seeding, and Server: \nhttps://youtu.be/38wDX84ZWVo \nA look into how the design was implemented.");
   } else if (lowerCaseTxt == "5") {
-    twiml.message("A Look at the Routers: \nhttps://youtu.be/rMie6X3tllk");
+    twiml.message("A Look at the Routers: \nhttps://youtu.be/rMie6X3tllk \nHere's where we start cooking with gas!");
   } else if (lowerCaseTxt == "6") {
-    twiml.message("Middleware: \nhttps://youtu.be/CR462IrIUEg");
+    twiml.message("Middleware: \nhttps://youtu.be/CR462IrIUEg \nIt's easier to catch errors at the start.");
   } else if (lowerCaseTxt == "7") {
-    twiml.message("Shared Functions: \nhttps://youtu.be/mRGok8xSWVw");
+    twiml.message("Shared Functions: \nhttps://youtu.be/mRGok8xSWVw \nMy favorite part of the code!");
   } else if (lowerCaseTxt == "8") {
-    twiml.message("Documentation: \nhttps://youtu.be/ow8mxgmvUgA");
+    twiml.message("Documentation: \nhttps://youtu.be/ow8mxgmvUgA \nOne of the most important steps!");
   } else if (lowerCaseTxt == "9") {
-    twiml.message("Testing: \nhttps://youtu.be/v8XhsUzHbQ0");
+    twiml.message("Testing: \nhttps://youtu.be/v8XhsUzHbQ0 \nTesting is key!");
   } else if (lowerCaseTxt == "10") {
-    twiml.message("What are the Next Steps? \nhttps://youtu.be/mAPKGvEJPYA");
+    twiml.message("What Are the Next Steps? \nhttps://youtu.be/mAPKGvEJPYA \nIt's important to have a roadmap for the future.");
   } else if (lowerCaseTxt == "11") {
-    twiml.message("About Me: \nhttps://youtu.be/1ZjRujVgKuY");
+    twiml.message("About Me: \nhttps://youtu.be/1ZjRujVgKuY \nA bit about myself and my story.");
   } else if (lowerCaseTxt == "?") {
     twiml.message(
       "Answer either of these riddles to gain access to the Secret Menu: \nIf you have it, you want to share it, but if you share it, it no longer exists. What is it? \nWhich Wonderful Company Draws the Owl?"
